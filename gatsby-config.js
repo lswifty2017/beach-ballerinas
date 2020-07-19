c14a315d7b3db5a4e78ef7db979550465a8f2771
Liam <l.swift94@gmail.com>
2020-07-19 15:01:14 +1000

    Changed up config for assets

--- gatsby-config.js
+++ gatsby-config.js
@@ -27,7 +27,7 @@ module.exports = {
     {
       resolve: "gatsby-source-filesystem",
       options: {
-        path: `${__dirname}/static/img`,
+        path: `${__dirname}/static/assets`,
         name: "uploads",
       },
     },
