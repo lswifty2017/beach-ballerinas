a73af4ed0cc39f9418b043bcf83d719aeefa12d6
Liam <l.swift94@gmail.com>
2020-07-26 13:47:38 +1000

    Changed gatsby config

--- gatsby-config.js
+++ gatsby-config.js
@@ -19,17 +19,17 @@ module.exports = {
       },
     },
     {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `content`,
-        path: `${__dirname}/content`,
-      },
-    },
-    {
       resolve: "gatsby-source-filesystem",
       options: {
         path: `${__dirname}/cms-assets`,
         name: "uploads",
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `content`,
+        path: `${__dirname}/content`,
       },
     },
     {
