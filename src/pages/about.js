af05a1e89c919293fcbe1786413f6d667b4c792c
Liam Swift <lswift@squiz.net>
2020-08-06 11:05:22 +1000

    Added more content and banner field to cms

--- src/pages/about.js
+++ src/pages/about.js
@@ -2,6 +2,28 @@ import React from "react"
 import Layout from "../components/layout/layout"
 import SEO from "../components/seo/seo"
 import Banner from "../components/banner/banner"
+import { useStaticQuery, graphql } from "gatsby"
+import GatsbyImage from "gatsby-image"
+
+// export const query = useStaticQuery(graphql`
+//   query MyQuery {
+//     staff: markdownRemark(fields: { slug: { regex: "/staff/" } }) {
+//       frontmatter {
+//         team {
+//           description
+//           name
+//           photo {
+//             childImageSharp {
+//               fluid {
+//                 srcSet
+//               }
+//             }
+//           }
+//         }
+//       }
+//     }
+//   }
+// `)
 
 const AboutPage = ({ data }) => {
   return (
