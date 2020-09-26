bc7734b0ca9788e22f0870da5907d0612eaec240
Liam <l.swift94@gmail.com>
2020-09-26 15:59:31 +1000

    Added favicon and manifest plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -84,6 +84,19 @@ module.exports = {
         },
       },
     },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: "Beach Ballerinas",
+        short_name: "Beach Ballerinas",
+        start_url: "https://beachballerinas.com.au",
+        // Enables "Add to Homescreen" prompt and disables browser UI (including back button)
+        // see https://developers.google.com/web/fundamentals/web-app-manifest/#display
+        display: "standalone",
+        icon: "src/assets/bb-favicon.png", // This path is relative to the root of the site.
+        crossOrigin: `use-credentials`,
+      },
+    },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     `gatsby-plugin-netlify-cms`,
