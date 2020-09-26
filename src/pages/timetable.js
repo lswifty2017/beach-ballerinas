6d62833c5ef02859f5d90ead4453b058fe8ff937
Liam <l.swift94@gmail.com>
2020-09-26 16:20:54 +1000

    Compressed assets

--- src/pages/timetable.js
+++ src/pages/timetable.js
@@ -1,6 +1,5 @@
 import React from "react"
 import { graphql } from "gatsby"
-import { paramCase } from "change-case"
 import moment from "moment"
 import Layout from "../components/layout/layout"
 import SEO from "../components/seo/seo"
